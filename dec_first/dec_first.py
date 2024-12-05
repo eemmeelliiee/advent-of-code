ab03de86b2cf3d151d78d66cfeb2ebf0e732c2fe
Emelie <emelievons@gmail.com>
2024-12-06 00:59:33 +0100

    Refactor

--- dec_first/dec_first.py
+++ dec_first/dec_first.py
@@ -1,19 +1,21 @@
 import pandas as pd
 
+# Path to Excel file
 excel_path = 'dec_first/data/dec_first_input.xlsx'
 
-left_col_df = pd.read_excel(excel_path)
-right_col_df = pd.read_excel(excel_path)
+# Read Excel file, and convert columns into seperate Pandas DataFrames
+left_col_df = pd.read_excel(excel_path, usecols='left_col')
+right_col_df = pd.read_excel(excel_path, usecols='right_col')
 
-left_col_name = 'left_col'
-right_col_name = 'right_col'
+# Convert DataFrames into Lists
+left_col_list = left_col_df.tolist()
+right_col_list = right_col_df.tolist()
 
-left_col = left_col_df[left_col_name]
-right_col = right_col_df[right_col_name]
+# Sort Lists by ascending order
+left_col_list.sort()
+right_col_list.sort()
 
-left_col_list = left_col.tolist()
-right_col_list = right_col.tolist()
-
+## Print lists
 print(left_col_list)
 print(right_col_list)
 
