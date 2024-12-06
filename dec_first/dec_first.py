3656d38b73e510c0e8931e1fb5dde8dbd8fde6b6
Emelie <emelievons@gmail.com>
2024-12-06 02:04:45 +0100

    Add logic

--- dec_first/dec_first.py
+++ dec_first/dec_first.py
@@ -4,10 +4,10 @@ import pandas as pd
 excel_path = 'dec_first/data/dec_first_input.xlsx'
 
 # Read Excel file, and convert columns into seperate Pandas DataFrames
-left_col_df = pd.read_excel(excel_path, usecols='left_col')
-right_col_df = pd.read_excel(excel_path, usecols='right_col')
+left_col_df = pd.read_excel(excel_path)['left_col']     # ", usecols='left_col')" would not work...
+right_col_df = pd.read_excel(excel_path)['right_col']   # ", usecols='right_col')" would not work...
 
-# Convert DataFrames into Lists
+# Convert Pandas DataFrames into Lists
 left_col_list = left_col_df.tolist()
 right_col_list = right_col_df.tolist()
 
@@ -15,7 +15,15 @@ right_col_list = right_col_df.tolist()
 left_col_list.sort()
 right_col_list.sort()
 
-## Print lists
-print(left_col_list)
-print(right_col_list)
+# For each index i in left_col_list, calculate the distance to corresponding index in right_col_list
+all_distances_list = []
+for i in range(len(left_col_list)):
+             distance = abs(left_col_list[i]-right_col_list[i])
+             all_distances_list.append(distance)
 
+# Sum all distances
+sum_all_distances = sum(all_distances_list)
+
+# Print result 
+print("Sum of all distances:")
+print(sum_all_distances)
